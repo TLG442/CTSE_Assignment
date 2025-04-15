53c289de73c6ab99cb540d769c978813edfb83ae
Tharuka Lakshan <tharukal@savinda.lk>
2025-04-15 22:38:56 +0530

    remove logger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const getLogger = require('./middleware/loggerMiddleware');
+
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 //load environment variables
@@ -18,8 +18,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//apply request logger
-app.use(getLogger());
+
+
 
 //routes
 app.use('/api/auth', require('./routes/authRoutes'));
@@ -30,9 +30,6 @@ app.get('/', (req, res) => {
   res.send(`API is running on porttttt ${process.env.PORT}...`);
 });
 
-// error handling middleware
-app.use(notFound);
-app.use(errorHandler);
 
 //start server
 const PORT = process.env.PORT || 5000;
